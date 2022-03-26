2dc866c5c83554fd22d3de2348101c5059a8bef4
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-03-26 12:58:31 +0530

    Update and rename chatbot to chatbot.js

--- database/chatbot.js
+++ database/chatbot.js
@@ -1,3 +1,5 @@
+function chatbot() { 
+
 var msgbot=[[[""],
 	     ["Have a nice day\nThank you"]],
 	    [["Hi"],
@@ -44,4 +46,32 @@ var msgbot=[[[""],
 	    [[],[]],
 	    [[],[]],
 	    [[],[]]]; 
-var mcnt=15;
+	
+	var mcnt=15;
+
+	var nbot = document.getElementById("inField").value; 
+	var mbot;
+	if (nbot.charCodeAt((nbot.length)-1)==(" ".charCodeAt(0))) { 
+		mbot = nbot.slice(0, (nbot.length)-1); 
+		// document.getElementById("div1").innerHTML += "<br>" + nbot +" "+ mbot; 
+	} else {mbot=nbot;}
+	var rbot; 
+	var b=false;
+	for(let i=0; i<mcnt; i++) { 
+		for(let j=0; j<1; j++) { 
+			/*
+			if (mbot.charCoadAt((mbot[i][0].length)-1)==63) {
+				for(let k=0; k<(msgbot[i][0].length)-2; k++) { 
+					var tmbot+=msgbot[i][0].charCoadAt(k); 
+				} 
+			} */
+			
+			if(mbot==msgbot[i][0]) { 
+			rbot=msgbot[i][1]; b=true; break; 
+			}
+		} 
+		if(b) {break;}
+	} 
+	if(!b) {rbot=msgbot[0][1];}
+	document.getElementById("div1").innerHTML += "<br>" + rbot; 
+}
