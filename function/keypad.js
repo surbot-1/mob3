e7a6475026bacc27d686dd92dcd8ed981a9add43
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-03-27 13:14:43 +0530

    Update keypad.js

--- function/keypad.js
+++ function/keypad.js
@@ -5,21 +5,21 @@ function drawKeypad(x, y) { alert('1');
   var kc=0; var kr=0; 
   var kl=8; var kt=8; 
   var kw=64; var kh=64; 
-  
+  alert('2');
   var cvs = document.getElementById("canvas"); 
   var ctx = cvs.getContext('2d'); 
   var imgData = ctx.createImageData(kw, kh); 
-  
+  alert('3');
   var imgBuf = new ArrayBuffer(kw*4*kh*krw*kcl); 
   var imgView = new Uint8Array(imgBuf); 
-  
+  alert('4');
   for (let i=0; i<kw*4*kh*krw*kcl; i+=4) { 
     imgViwe[i+0] = 0xFF; 
     imgViwe[i+1] = 0x00; 
     imgViwe[i+2] = 0x00; 
     imgViwe[i+3] = 0xFF; 
   } 
-  
+  alert('5');
   function draw(kc, kr, kw, kh, kl, kt) { 
     var kx=x+(kw+kl*2)*kc; 
     var ky=y+(kh+kt*2)*kr; 
@@ -28,7 +28,7 @@ function drawKeypad(x, y) { alert('1');
     } 
     ctx.putImageData(imgData, kx, ky);
   } 
-  
+  alert('6');
   for (let j=0; j<krw; j++) { 
     for (let i=0; i<kcl; i++) { 
       if (i==0 && j==0) {} 
@@ -39,5 +39,5 @@ function drawKeypad(x, y) { alert('1');
       draw(kc, kr, kw, kh, kl, kt); 
     } 
   }  
-  
+  alert('7');
 }
